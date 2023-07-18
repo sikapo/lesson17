0117933d713393ef508a009c8a5729fe1b3945cc
Owner <vitycity79@gmail.com>
2023-07-18 15:48:07 +0300

    solution

--- src/LeetCodeTusk122/Main.java
+++ src/LeetCodeTusk122/Main.java
@@ -1,4 +1,28 @@
 package LeetCodeTusk122;
 
 public class Main {
+    public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                int profit = price - minPrice;
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{2,1,4,0,1}));
+    }
 }
